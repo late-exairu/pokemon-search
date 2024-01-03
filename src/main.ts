e0deca6dc8525495f6be0568ddd5a4657b7cd823
Oleg Shelest <lateexairu@gmail.com>
2024-01-03 14:26:09 +0200

    Add search functionality and display Pokémon details

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,65 @@ const searchButton = document.querySelector(
   "#search-button",
 ) as HTMLButtonElement;
 const searchInput = document.querySelector("#search-input") as HTMLInputElement;
-const result = document.querySelector("#result") as HTMLDivElement;
+const form = document.querySelector("#search-input") as HTMLFormElement;
+const card = document.querySelector("#card") as HTMLDivElement;
+const pokemonName = document.querySelector("#pokemon-name") as HTMLSpanElement;
+const pokemonId = document.querySelector("#pokemon-id") as HTMLSpanElement;
+const weight = document.querySelector("#weight") as HTMLSpanElement;
+const height = document.querySelector("#height") as HTMLSpanElement;
+const spriteContainer = document.querySelector(
+  "#sprite-container",
+) as HTMLDivElement;
+const types = document.querySelector("#types") as HTMLDivElement;
+const hp = document.querySelector("#hp") as HTMLTableCellElement;
+const attack = document.querySelector("#attack") as HTMLTableCellElement;
+const defense = document.querySelector("#defense") as HTMLTableCellElement;
+const specialAttack = document.querySelector(
+  "#special-attack",
+) as HTMLTableCellElement;
+const specialDefense = document.querySelector(
+  "#special-defense",
+) as HTMLTableCellElement;
+const speed = document.querySelector("#speed") as HTMLTableCellElement;
 
-const handleCheck = (e: Event) => {
+const handleSearch = (e: Event) => {
   e.preventDefault();
+  const query = searchInput.value;
+  if (!query) return;
+
+  fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${query}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      pokemonName.textContent = data.name;
+      pokemonId.textContent = `#${data.id}`;
+      weight.textContent = `Weight: ${data.weight}`;
+      height.textContent = `Height: ${data.height}`;
+      types.innerHTML = "";
+      data.types.forEach((type: any) => {
+        types.innerHTML += `<span class="type ${type.type.name}">${type.type.name}</span>`;
+      });
+      hp.textContent = data.stats[0].base_stat;
+      attack.textContent = data.stats[1].base_stat;
+      defense.textContent = data.stats[2].base_stat;
+      specialAttack.textContent = data.stats[3].base_stat;
+      specialDefense.textContent = data.stats[4].base_stat;
+      speed.textContent = data.stats[5].base_stat;
+
+      spriteContainer.innerHTML = "";
+      const image = document.createElement("img");
+      image.src = data.sprites.front_default;
+      image.alt = data.name;
+      image.classList.add("mx-auto", "w-[192px]");
+      spriteContainer.appendChild(image);
+      card.classList.remove("hidden");
+    })
+    .catch((err) => {
+      alert("Pokémon not found");
+      console.error(err);
+      card.classList.add("hidden");
+    });
 };
 
-searchButton?.addEventListener("click", handleCheck);
+form?.addEventListener("submit", handleSearch);
+searchButton?.addEventListener("click", handleSearch);
